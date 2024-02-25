97d1527aba2542258c4fd6abdc1b16ee764cd590
brittanyshelley <brittany.j.shelley@gmail.com>
2024-02-25 16:50:16 -0700

    Add Inter font and Tailwind antialiased class to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Inter font will be applied throughout app since it's in body, Tailwind antialiased class
+      smooths out the font rendering */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
